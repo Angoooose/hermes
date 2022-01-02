94665be83cea6bfe5f5175671f53e09ed2528eeb
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-01-02 14:32:29 -0600

    Adding Firebase support

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+import { initializeApp } from 'firebase/app'; 
+import firebaseConfig from './firebase.config';
+
+let app = initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
