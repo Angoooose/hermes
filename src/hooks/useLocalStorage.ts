b82ce94bba24380eb623d7094a9ca3839b64995e
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-01-11 12:51:43 -0600

    Improving useLocalStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-export default function useLocalStorage<T>(key: string): [T, typeof setValue, typeof clearValue] {
-    const [storedValue, setStoredValue] = useState<T>(localStorage.getItem(key) as unknown as T);
+export default function useLocalStorage<T>(key: string): [T|undefined, typeof setValue, typeof clearValue] {
+    const [storedValue, setStoredValue] = useState<T>();
+
+    useEffect(() => {
+        setStoredValue(localStorage.getItem(key) as unknown as T);
+    }, []);
 
     const setValue = (value: T) => {
         localStorage.setItem(key, value as unknown as string);
