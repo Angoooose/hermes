f1b9edc14d453511c3eb42910ab29ca9f9fb5c0e
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-01-13 11:05:48 -0600

    header-icon.png -> logo.png

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
 import '../styles/Header.css';
 
-import headerImage from '../assets/header-icon.png';
+import logoImage from '../assets/logo.png';
 import githubImage from '../assets/GitHub-Mark-Light-120px-plus.png';
 
 export default function Header() {
     return (
         <header>
             <a className="header-title" href="/">
-                <img className="header-img" src={headerImage} alt="Chat Bubble"/>
+                <img className="header-img" src={logoImage} alt="Chat Bubble"/>
                 hermes
             </a>
             <a href="https://github.com/Angoooose/hermes" target="_blank" rel="noreferrer" className="header-link">
