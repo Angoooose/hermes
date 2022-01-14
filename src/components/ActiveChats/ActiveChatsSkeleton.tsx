68605c927c6789b0936be11eb4822af8b2e6b75c
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-01-13 19:29:24 -0600

    Improving ActiveChatsSkeleton component

--- src/components/ActiveChats/ActiveChatsSkeleton.tsx
+++ src/components/ActiveChats/ActiveChatsSkeleton.tsx
@@ -1,8 +1,18 @@
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 
-// This will be improved much more later, just needed to push to switch computers.
-
 export default function ActiveChatsSkeleton() {
+    function ChatCardSkeleton() {
+        return (
+            <div className="active-chat-card">
+                <Skeleton style={{ width: 150, height: 30 }}/>
+                <div className="active-chat-right">
+                    <Skeleton width={75}/>
+                    <Skeleton width={100} height={30} style={{ borderRadius: 'var(--msg-right-border-radius)' }}/>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className="active-chats-container">
             <h1>Your active chats</h1>
@@ -10,46 +20,12 @@ export default function ActiveChatsSkeleton() {
                 <SkeletonTheme baseColor="#282d42" highlightColor="#464f74" >
                     <div className="active-chats-account-info">
                         <div>
-                            <Skeleton style={{ width: 125}}/>
-                            <Skeleton style={{ width: 180, height: 50, marginTop: 5}}/>
+                            <Skeleton width={125}/>
+                            <Skeleton width={180} height={50} style={{ marginTop: 5 }}/>
                         </div>
-                        <Skeleton style={{ width: 80, height: 40, marginRight: 10 }}/>
+                        <Skeleton width={80} height={40} style={{ marginRight: 10 }}/>
                     </div>
-                    <div className="active-chat-card">
-                        <Skeleton style={{ width: 150, height: 30 }}/>
-                        <div className="active-chat-right">
-                            <Skeleton style={{ width: 50 }}/>
-                            <Skeleton style={{ width: 100, height: 30, borderRadius: '12px 12px 0 12px' }}/>
-                        </div>
-                    </div>
-                    <div className="active-chat-card">
-                        <Skeleton style={{ width: 150, height: 30 }}/>
-                        <div className="active-chat-right">
-                            <Skeleton style={{ width: 50 }}/>
-                            <Skeleton style={{ width: 100, height: 30, borderRadius: '12px 12px 0 12px' }}/>
-                        </div>
-                    </div>
-                    <div className="active-chat-card">
-                        <Skeleton style={{ width: 150, height: 30 }}/>
-                        <div className="active-chat-right">
-                            <Skeleton style={{ width: 50 }}/>
-                            <Skeleton style={{ width: 100, height: 30, borderRadius: '12px 12px 0 12px' }}/>
-                        </div>
-                    </div>
-                    <div className="active-chat-card">
-                        <Skeleton style={{ width: 150, height: 30 }}/>
-                        <div className="active-chat-right">
-                            <Skeleton style={{ width: 50 }}/>
-                            <Skeleton style={{ width: 100, height: 30, borderRadius: '12px 12px 0 12px' }}/>
-                        </div>
-                    </div>
-                    <div className="active-chat-card">
-                        <Skeleton style={{ width: 150, height: 30 }}/>
-                        <div className="active-chat-right">
-                            <Skeleton style={{ width: 50 }}/>
-                            <Skeleton style={{ width: 100, height: 30, borderRadius: '12px 12px 0 12px' }}/>
-                        </div>
-                    </div>
+                    {Array(5).fill(null).map(() => <ChatCardSkeleton/>)}
                 </SkeletonTheme>
             </div>
         </div>
